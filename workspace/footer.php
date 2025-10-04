2502b029ae51e74bd34d822402833174be97f827
soulution solution <solutionsoulution@gmail.com>
2025-10-04 13:59:26 +0000

    maintenant mettez ce lien dans le theme wordpress là où il y a le lien d

--- workspace/footer.php
+++ workspace/footer.php
@@ -57,6 +57,11 @@
         </div>
     </div>
 </footer>
+
+<div class="gtranslate_wrapper"></div>
+<script>window.gtranslateSettings = {"default_language":"fr","native_language_names":true,"languages":["fr","it","es","de","pt","en"],"wrapper_selector":".gtranslate_wrapper","switcher_horizontal_position":"right"}</script>
+<script src="https://cdn.gtranslate.net/widgets/latest/float.js" defer></script>
+
 <?php wp_footer(); ?>
 </body>
 </html>
--- workspace/header.php
+++ workspace/header.php
@@ -22,14 +22,14 @@
 <header class="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
     <div class="container flex h-16 max-w-screen-2xl items-center">
         <a href="<?php echo esc_url(home_url('/')); ?>" class="mr-6 flex items-center space-x-2">
-            <span class="font-bold sm:inline-block">VylsFond</span>
+            <span class="font-bold sm:inline-block text-xl">VylsFond</span>
         </a>
         
         <!-- L'espace pour le menu est laissé vide pour être géré par un plugin -->
         <div class="flex-1"></div>
 
         <div class="flex items-center justify-end space-x-2">
-            <a href="https://google.com" target="_blank" rel="noopener noreferrer" class="border border-input bg-background hover:bg-accent hover:text-accent-foreground inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium h-10 px-4 py-2">Espace Client</a>
+            <a href="http://banque.vylsfond.com" target="_blank" rel="noopener noreferrer" class="border border-input bg-background hover:bg-accent hover:text-accent-foreground inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium h-10 px-4 py-2">Espace Client</a>
             <a href="/demande-de-pret" class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2">Faire une demande</a>
         </div>
     </div>
