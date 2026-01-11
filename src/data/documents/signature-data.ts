c29d8100bd7663dbbb14f15fb341372f3f450051
soulution solution <solutionsoulution@gmail.com>
2026-01-11 20:53:24 +0000

    mettez la dernière signature que je vous ai envoyé pour Klaus Schmidt

--- src/data/documents/signature-data.ts
+++ src/data/documents/signature-data.ts
@@ -5,7 +5,7 @@ export const signatureData = (brandKey: 'neofonds' | 'finarcy' | 'vantex') => {
             ceo: {
                 name: "Klaus Schmidt",
                 title: { fr: "Directeur Général", en: "CEO", de: "Geschäftsführer", lt: "Generalinis direktorius", nl: "Algemeen Directeur" },
-                signatureUrl: "https://i.postimg.cc/76wJbHLK/Capture-d-ecran-2025-12-20-110200.png"
+                signatureUrl: "https://i.postimg.cc/jq1H8SMP/signature-5.png"
             },
             legal: {
                 name: "Markus Weber",
